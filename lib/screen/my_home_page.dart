52e064a13cea0f6aa34768919a146da32f8590c2
Pipop Intapa <pdi.pipop@gmail.com>
2024-05-16 17:42:25 +0700

    comp

--- lib/screen/my_home_page.dart
+++ lib/screen/my_home_page.dart
@@ -1,30 +1,59 @@
+
 import 'package:flutter/material.dart';
+
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
+
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-  void _incrementCounter(){
+  int _conter = 0;
+
+  void _adderCount() {
     setState(() {
-      _counter++;
+      _conter++;
     });
   }
+
+  void _subtractorCount() {
+    if (_conter > 0) {
+      setState(() {
+        _conter--;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("My incremen counter")
+        title: const Text("My Counter"),
       ),
-      body: Center(child: Text("นับไปแล้ว $_counter ครั้ง"),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        child: const Icon(Icons.add),
-        
-      ),
+      body: Center(
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+            Text("จำนวนครั้งที่นับไปแล้ว $_conter ครั้ง",
+                style: TextStyle(fontSize: 20, color: Color.fromARGB(255, 39, 2, 109))),
+            Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
+              ElevatedButton(
+                onPressed: _subtractorCount,
+                child: Text(
+                  "-",
+                  style: TextStyle(fontSize: 50, color: Color.fromARGB(255, 190, 3, 3)),
+                ),
+              ),
+              ElevatedButton(
+                onPressed: _adderCount,
+                child: Text(
+                  "+",
+                  style: TextStyle(fontSize: 50, color: Color.fromARGB(255, 0, 0, 0)),
+                ),
+              ),
+            ]),
+          ])),
     );
   }
 }
